b89d1c3c852b09fb4ef304701cee3c187723be2d
Faiz <faizrocx9211@gmail.com>
2020-11-30 17:36:24 +0530

    Basic Djanfo setup with minimilastic internal database

---
+++ courses/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Course
+
+#The below base implemented class just creates a normal searilaizers 
+
+# class CourseSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Course
+#         fields = ('id','name','language','price')
+
+
+#The below base implemented class just creates a hyperlink searilaizers
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('id','name','language','price','url')
